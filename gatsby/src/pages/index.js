81e3de87c08dc6c95e1654e4273428fad8d05eaa
maniac23 <tukaym@gmail.com>
2020-12-05 15:33:05 +0300

    Add initial pages

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function HomePage() {
+  return <div>
+    <p>Hello, I'm a home page</p>
+    </div>
+}
---
+++ gatsby/src/pages/order.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+export default function OrderPage() {
+  return <p>Hey! I'm order page</p>
+}
