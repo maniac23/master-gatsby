0444f4789f4efb75a3774a72cc543c1ac55cab6c
maniac23 <tukaym@gmail.com>
2020-12-05 18:16:39 +0300

    custom preview of pizzas

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
-import {MdLocalPizza as icon} from 'react-icons/md'
+import { string } from 'prop-types';
+import { MdLocalPizza as icon } from 'react-icons/md';
 
 export default {
   name: 'pizza', // computer name
@@ -10,7 +11,7 @@ export default {
       name: 'name',
       title: 'Pizza name',
       type: 'string',
-      description: "Name of the pizza"
+      description: 'Name of the pizza'
     },
     {
       name: 'slug',
@@ -34,8 +35,32 @@ export default {
       title: 'Price',
       type: 'number',
       description: 'Price of the pizza in cents',
-      validation: Rule => Rule.min(1000).max(50000)
+      validation: (Rule) => Rule.min(1000).max(50000)
       // TODO: add custom input component
     },
-  ]
-}
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name'
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      const tops = Object.values(toppings).filter(Boolean);
+      return {
+        title,
+        media,
+        subtitle: tops.join(', ')
+      };
+    }
+  }
+};
