2248fa4b051ddf82b3671d284df44e4341b72c9d
maniac23 <tukaym@gmail.com>
2020-12-05 15:40:11 +0300

    Added navigation

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function HomePage() {
-  return <div>
-    <p>Hello, I'm a home page</p>
+  return (
+    <div>
+      <Nav />
+      <p>Hello, I'm a home page</p>
     </div>
+  );
 }
