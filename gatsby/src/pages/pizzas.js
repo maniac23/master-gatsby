5bedcaf06b3e1ca246f4ee551049c4cae9cf1ea8
maniac23 <tukaym@gmail.com>
2020-12-06 15:34:57 +0300

    Added pizzas query and image

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,5 +1,41 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
-  return <p>Hey! I'm pizzas page</p>;
+export default function PizzasPage({ data }) {
+  console.log(data);
+  const pizzas = data.pizzas.nodes;
+  return (
+    <>
+      <PizzaList pizzas={pizzas} />
+    </>
+  );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
