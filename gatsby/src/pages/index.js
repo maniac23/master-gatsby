25e6d543a1c0bcd443563ebd81ce45b2c21c916b
maniac23 <tukaym@gmail.com>
2020-12-12 19:47:27 +0300

    client side data fetching

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentylSlicing() {
+  return (
+    <div>
+      <p>CurrentylSlicing</p>
+    </div>
+  );
+}
+
+function HotSlices() {
+  return (
+    <div>
+      <p>Hot slices</p>
+    </div>
+  );
+}
 
 export default function HomePage() {
-  return <p>Hello, I'm a home page</p>;
+  const { slicemasters, hotSlices } = useLatestData();
+
+  return (
+    <div className="center">
+      <h1>The Best pizza Downtown</h1>
+      <p>Open 11am to 11pm Every single day</p>
+      <CurrentylSlicing slicemasters={slicemasters} />
+      <HotSlices hotSlices={hotSlices} />
+    </div>
+  );
 }
