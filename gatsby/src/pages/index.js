d44dc8fc857ee28e658649ad67471f4721e4e116
maniac23 <tukaym@gmail.com>
2020-12-13 14:51:30 +0300

    added loading skeleton for homepage

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import useLatestData from '../utils/useLatestData';
+import { HomepageGrid } from '../styles/Grids';
+import LoadingGrid from '../components/LoadingGrid';
 
-function CurrentylSlicing() {
+function CurrentylSlicing({ slicemasters }) {
   return (
     <div>
-      <p>CurrentylSlicing</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p> No one is working right now </p>
+      )}
+      {/* <p>Currentyl Slicing</p> */}
     </div>
   );
 }
 
-function HotSlices() {
+function HotSlices({ hotSlices }) {
   return (
     <div>
-      <p>Hot slices</p>
+      {/* <p>Hot slices</p> */}
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p> Nothing in the case </p>}
     </div>
   );
 }
@@ -24,8 +32,10 @@ export default function HomePage() {
     <div className="center">
       <h1>The Best pizza Downtown</h1>
       <p>Open 11am to 11pm Every single day</p>
-      <CurrentylSlicing slicemasters={slicemasters} />
-      <HotSlices hotSlices={hotSlices} />
+      <HomepageGrid>
+        <CurrentylSlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomepageGrid>
     </div>
   );
 }
